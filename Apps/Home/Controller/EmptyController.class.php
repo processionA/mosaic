c49f75484313bafdc27161a6e3840367d6a8da45
bigo <765840389@qq.com>
2017-02-24 16:04:28 +0800

    github2-24

---
+++ Apps/Home/Controller/EmptyController.class.php
@@ -0,0 +1,19 @@
+<?php
+namespace Home\Controller;
+use Think\Controller;
+class EmptyController extends Controller {
+
+    // 访问不存在的方法自动调用
+    public function _empty($key,$val)
+    {
+        $this->redirect('Home/Empty/error');
+    }
+
+    public function error()
+    {
+    	$this->display('error');
+    }
+
+
+}
+ 
